cc1159d137bdd9ad02266b79655d363f29275d7c
Marko Milanovic <markomilanovi02@gmail.com>
2022-07-23 20:26:00 +0200

    Project finished

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,16 @@
   <h1>Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame" @curr="currentTime" />
-  <p v-if="showResults">Reaction time: {{ score }}</p>
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
+import Results from "./components/Results.vue"
 import Block from "./components/Block.vue"
 
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
@@ -43,4 +44,19 @@ export default {
   color: #444;
   margin-top: 60px;
 }
+button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
 </style>
